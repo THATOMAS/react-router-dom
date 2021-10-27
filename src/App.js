b0afb31dc1128f861d43714db8cbd630fc2ce886
THATOMAS <chachumashori@gmail.com>
2021-10-27 19:21:01 +0200

    Editted the Html file to add a new icon thats more suitable,tempered with App.js file to add a bit more detail ,also added a button and styled it appropiatly.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react"
-import data from "./data"
-import "./index.css"
+import React, { useState } from "react";
+import List from "./List";
+import data from "./data";
+import "./index.css";
 
 function App() {
 
   return <>
     <main>
-      <div className="container">
 
-      </div>
+      <section className="container">
+        <h3>Birthdays today</h3>
+        <List />
+        <button onClick={() => console.log("You clicked me")}>CLEAR ALL</button>
+      </section>
 
     </main>
 
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const List = () => {
+
+
+    return <>
+        <h3>List component</h3>
+
+    </>
+}
+
+export default List;
